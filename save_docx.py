80c7ab5e1b93922a083dd1912b67d60edb3f9266
2877267169@qq.com <2877267169@qq.com>
2020-10-27 23:20:45 +0800

    修正生成报告内图片尺寸的问题

--- save_docx.py
+++ save_docx.py
@@ -2,9 +2,10 @@ import os
 
 from PyQt5.QtWidgets import QMessageBox, QFileDialog
 import docx
-
+from docx.shared import Cm
 import MainWindow
 from main_window_run import my_app_img_dir
+
 
 def save_docx(ui: MainWindow.Ui_MainWindow):
     """
@@ -14,14 +15,14 @@ def save_docx(ui: MainWindow.Ui_MainWindow):
     """
     base_dir = my_app_img_dir
     if (
-        os.path.isfile(os.path.join(base_dir, "corpus.png")) and
-        os.path.isfile(os.path.join(base_dir, "train_res.png")) and
-        os.path.isfile(os.path.join(base_dir, "ana_A.png")) and
-        os.path.isfile(os.path.join(base_dir, "ana_B.png")) and
-        os.path.isfile(os.path.join(base_dir, "ana_C.png")) and
-        os.path.isfile(os.path.join(base_dir, "ana_D.png")) and
-        os.path.isfile(os.path.join(base_dir, "warning.png")) and
-        os.path.isfile(os.path.join(base_dir, "word_cloud.png"))
+            os.path.isfile(os.path.join(base_dir, "corpus.png")) and
+            os.path.isfile(os.path.join(base_dir, "train_res.png")) and
+            os.path.isfile(os.path.join(base_dir, "ana_A.png")) and
+            os.path.isfile(os.path.join(base_dir, "ana_B.png")) and
+            os.path.isfile(os.path.join(base_dir, "ana_C.png")) and
+            os.path.isfile(os.path.join(base_dir, "ana_D.png")) and
+            os.path.isfile(os.path.join(base_dir, "warning.png")) and
+            os.path.isfile(os.path.join(base_dir, "word_cloud.png"))
     ) is True:
         filename_choose, _ = QFileDialog.getSaveFileName(
             ui.statusbar,
@@ -35,30 +36,44 @@ def save_docx(ui: MainWindow.Ui_MainWindow):
         for i, p in enumerate(doc.paragraphs):
             if '.图1.' in p.text:
                 p.runs[0].add_break()  # 添加一个折行
-                p.runs[0].add_picture(os.path.join(base_dir, "corpus.png"))  # 在runs的最后一段文字后添加图片
+                p.runs[0].add_picture(os.path.join(base_dir, "corpus.png"), width=Cm(14.64))  # 在runs的最后一段文字后添加图片
+                p.runs[0].add_break()  # 添加一个折行
             if '.图2.' in p.text:
                 p.runs[0].add_break()  # 添加一个折行
-                p.runs[0].add_picture(os.path.join(base_dir, "train_res.png"))
+                p.runs[0].add_picture(os.path.join(base_dir, "train_res.png"), width=Cm(14.64))
+                p.runs[0].add_break()  # 添加一个折行
             if '.图3.' in p.text:
                 p.runs[0].add_break()  # 添加一个折行
-                p.runs[0].add_picture(os.path.join(base_dir, "ana_A.png"))
+                p.runs[0].add_picture(os.path.join(base_dir, "ana_A.png"), width=Cm(14.64))
+                p.runs[0].add_break()  # 添加一个折行
             if '.图4.' in p.text:
                 p.runs[0].add_break()  # 添加一个折行
-                p.runs[0].add_picture(os.path.join(base_dir, "ana_C.png"))
+                p.runs[0].add_picture(os.path.join(base_dir, "ana_C.png"), width=Cm(14.64))
+                p.runs[0].add_break()  # 添加一个折行
             if '.图5.' in p.text:
                 p.runs[0].add_break()  # 添加一个折行
-                p.runs[0].add_picture(os.path.join(base_dir, "ana_B.png"))
+                p.runs[0].add_picture(os.path.join(base_dir, "ana_B.png"), width=Cm(8))
+                p.runs[0].add_break()  # 添加一个折行
             if '.图6.' in p.text:
                 p.runs[0].add_break()  # 添加一个折行
-                p.runs[0].add_picture(os.path.join(base_dir, "ana_D.png"))
+                p.runs[0].add_picture(os.path.join(base_dir, "ana_D.png"), width=Cm(8))
+                p.runs[0].add_break()  # 添加一个折行
             if '.图7.' in p.text:
                 p.runs[0].add_break()  # 添加一个折行
-                p.runs[0].add_picture(os.path.join(base_dir, "warning.png"))
+                p.runs[0].add_picture(os.path.join(base_dir, "warning.png"), width=Cm(14.64))
+                p.runs[0].add_break()  # 添加一个折行
             if '.图8.' in p.text:
                 p.runs[0].add_break()  # 添加一个折行
-                p.runs[0].add_picture(os.path.join(base_dir, "word_cloud.png"))
-        doc.save(filename_choose)
+                p.runs[0].add_picture(os.path.join(base_dir, "word_cloud.png"), width=Cm(14.64))
+                p.runs[0].add_break()  # 添加一个折行
+        try:
+            doc.save(filename_choose)
+            QMessageBox.information(ui.statusbar, "完成", "成功完成了生成", QMessageBox.Ok)
+
+        except:
+            QMessageBox.critical(
+                ui.statusbar, "Error", "发生了一些错误\n请检查是否有足够的权限访问，或目标文件处于是否处于占用状态。"
+            )
     else:
         QMessageBox.critical(ui.statusbar, "Error", "文件不完整。")
         print("文件不完整")
-
